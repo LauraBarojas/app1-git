07cb9f003d8f39240085433792f3dca6a98acaaf
Laura Barojas Arias <laurabarojasa@gmail.com>
2019-03-21 13:52:07 -0600

    3

--- js/acciones.js
+++ js/acciones.js
@@ -5,7 +5,7 @@
 		 $('#disp table td').eq(3).text(device.model);
 		 $('#disp table td').eq(5).text(device.cordova);
 		 $('#disp table td').eq(7).text(device.platform);
-		 $('#disp table td').eq(9).text(devuce.version);
+		 $('#disp table td').eq(9).text(device.version);
 		 $('#disp table td').eq(11).text(device.uuid);
 	 },false);//ready device
 	 
